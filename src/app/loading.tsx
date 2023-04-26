3dc7044e57cb34981abd1f798462cf48dc929976
Alex Xavier <alexjr.123@hotmail.com>
2023-04-25 21:49:18 -0300

    Implementa a Tela de loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { Flex, Heading } from '@chakra-ui/react'
+
+export default function Loading() {
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      bg="red.300"
+      color="white"
+      align="center"
+      justify="center"
+    >
+      <Heading fontSize="2rem">MyFood</Heading>
+      <Heading fontSize="1rem">Caregando</Heading>
+    </Flex>
+  )
+}
